f657f003e48401c6c203d95b2c0b33bcfdf1b066
TEMFACK DERICK <dericktemfack@gmail.com>
2022-06-20 20:00:00 +0100

    Change logo title

--- docs/conf.py
+++ docs/conf.py
@@ -97,8 +97,12 @@ html_theme = 'pydata_sphinx_theme'
 #
 # html_theme_options = {}
 html_theme_options = {
-  "show_nav_level": 2
+  "show_nav_level": 2,
+  "logo": {
+        "text": "Credit Risk Validation Tests",
+    }
 }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
