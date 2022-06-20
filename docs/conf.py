cac921d77ffbef1bdf3dbf3aff065164a9bf87a2
TEMFACK DERICK <dericktemfack@gmail.com>
2022-06-20 17:49:50 +0100

    Install pydata sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ release = risktests.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -88,7 +88,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
